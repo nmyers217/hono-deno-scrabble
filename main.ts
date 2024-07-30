da9bd25e75450a1c217a104139cb631f3458e1f9
nmyers217 <nickbmyers217@gmail.com>
2024-07-30 18:54:35 -0500

    Chore: initial commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/deno'
+
+const app = new Hono()
+
+app.use('/static/*', serveStatic({ root: './' }))
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+Deno.serve({
+  port: Deno.env.get('PORT') ? parseInt(Deno.env.get('PORT')!) : 8000,
+  handler: app.fetch,
+});
