eb48454248d462cfb84c4edbf394ee5d2dcf8fc3
nmyers217 <nickbmyers217@gmail.com>
2024-07-30 20:09:34 -0500

    Chore: work on auth config some more

--- main.ts
+++ main.ts
@@ -8,6 +8,34 @@ import {
   verifyAuth,
 } from "npm:@hono/auth-js";
 import GitHub from "npm:@auth/core/providers/github";
+import Google from "npm:@auth/core/providers/google";
+import Discord from "npm:@auth/core/providers/discord";
+import Credentials from "npm:@auth/core/providers/credentials";
+
+// TODO: Implement salt and hash password function
+function saltAndHashPassword(password: string): string {
+  return password;
+}
+
+// TODO: get a real db
+interface User {
+  id: string;
+  email: string;
+  password: string;
+}
+const users: Record<string, User> = {
+  "nick@example.com": {
+    id: "0",
+    email: "nick@example.com",
+    password: "123abc",
+  },
+};
+const db = {
+  users,
+  findUser: async (email: string, pwHash: string) => {
+    return db.users[email]?.password === pwHash ? db.users[email] : null;
+  },
+};
 
 const app = new Hono();
 
@@ -36,8 +64,26 @@ function getAuthConfig(c: Context): AuthConfig {
     secret: Deno.env.get("AUTH_SECRET"),
     providers: [
       GitHub({
-        clientId: c.env.GITHUB_ID,
-        clientSecret: c.env.GITHUB_SECRET,
+        clientId: Deno.env.get("GITHUB_ID")!,
+        clientSecret: Deno.env.get("GITHUB_SECRET")!,
+      }),
+      Google({
+        clientId: Deno.env.get("GOOGLE_ID")!,
+        clientSecret: Deno.env.get("GOOGLE_SECRET")!,
+      }),
+      Discord({
+        clientId: Deno.env.get("DISCORD_ID")!,
+        clientSecret: Deno.env.get("DISCORD_SECRET")!,
+      }),
+      Credentials({
+        credentials: {
+          email: {},
+          password: {},
+        },
+        authorize: async (credentials) => {
+          const pwHash = saltAndHashPassword(credentials.password as string);
+          return await db.findUser(credentials.email as string, pwHash);
+        },
       }),
     ],
   };
