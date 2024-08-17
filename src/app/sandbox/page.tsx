fb4034dc2b4591b8a99d908bfeb1cb40f054b382
nmyers217 <nickbmyers217@gmail.com>
2024-08-17 18:38:51 -0500

    Feat: start working on a tray to hold letters

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -5,7 +5,7 @@ import ScrabbleTile from "~/components/ui/scrabble-tile";
 export default function SandboxPage() {
   return (
     <main className="container mt-12 flex flex-col gap-4">
-      <Card className="w-[350px]">
+      <Card>
         <CardHeader>
           <CardTitle>Tiles</CardTitle>
         </CardHeader>
@@ -24,6 +24,34 @@ export default function SandboxPage() {
           <ScrabbleTile letter="F" size={5} theme="gruvbox" />
         </CardContent>
       </Card>
+
+      <div className="flex flex-row justify-between gap-4">
+        <Card className="w-6/12">
+          <CardHeader>
+            <CardTitle>Tray</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ScrabbleTray letters={["A", "B"]} />
+          </CardContent>
+        </Card>
+
+        <Card className="w-6/12">
+          <CardHeader>
+            <CardTitle>Board</CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
     </main>
+  );
+}
+
+export function ScrabbleTray(props: { letters?: string[] }) {
+  return (
+    <div className="flex w-full flex-row justify-center gap-2 rounded-lg border-2 p-2">
+      {props.letters?.map((letter, index) => (
+        <ScrabbleTile key={index} letter={letter} theme="gruvbox" />
+      ))}
+    </div>
   );
 }
